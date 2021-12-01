b3f1b9bdf6c62aef7401fa7f11aec05c3a912ca5
Catelyn <github@catelyn.tech>
2021-12-01 16:45:49 +0100

    updated run script

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
 @echo off
+
+if "%1"=="" (
+    echo too few arguments
+    exit /b 1
+)
+
 cls
+
 if %1==all (
     for %%s in (day??.c) do (
         gcc %%~ns.c -o %%~ns.exe -Wpedantic -Wall -Werror && %%~ns
