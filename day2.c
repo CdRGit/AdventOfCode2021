f0bf9c53ce9e50bd93b43486e39211cde9393d99
Catelyn <github@catelyn.tech>
2021-12-02 11:15:03 +0100

    day 2 solution

---
+++ day2.c
@@ -0,0 +1,91 @@
+#include "utils.h"
+
+int part1(char **lines);
+int part2(char **lines);
+
+void run(bool real)
+{
+    char **lines = get_lines_from_file(real ? "day2.txt" : "day2_test.txt");
+
+    printf("Part 1\n");
+    printf("= %u\n", part1(lines));
+
+    printf("Part 2\n");
+    printf("= %u\n", part2(lines));
+}
+
+int main(int argc, char **argv)
+{
+    printf("%s", argv[0]);
+    printf("\nTEST:\n");
+    run(false);
+    printf("\nREAL:\n");
+    run(true);
+}
+
+int part1(char **lines)
+{
+    int x = 0, depth = 0;
+
+    for (int i = 0; lines[i] != NULL; i++)
+    {
+        char* line = lines[i];
+        char* space = strchr(line, ' ');
+        if (space == NULL)
+        {
+            fprintf(stderr, "SPACE NOT FOUND ON LINE %d\n", i + 1);
+            exit(1);
+        }
+        char* num = space + 1;
+        int value = to_int(num);
+        char first = *line;
+        switch (first)
+        {
+            case 'f':
+                x += value;
+                break;
+            case 'u':
+                depth -= value;
+                break;
+            case 'd':
+                depth += value;
+                break;
+        }
+    }
+
+    return x * depth;
+}
+
+int part2(char **lines)
+{
+    int x = 0, depth = 0, aim = 0;
+
+    for (int i = 0; lines[i] != NULL; i++)
+    {
+        char* line = lines[i];
+        char* space = strchr(line, ' ');
+        if (space == NULL)
+        {
+            fprintf(stderr, "SPACE NOT FOUND ON LINE %d\n", i + 1);
+            exit(1);
+        }
+        char* num = space + 1;
+        int value = to_int(num);
+        char first = *line;
+        switch (first)
+        {
+            case 'f':
+                x += value;
+                depth += value * aim;
+                break;
+            case 'u':
+                aim -= value;
+                break;
+            case 'd':
+                aim += value;
+                break;
+        }
+    }
+
+    return x * depth;
+}
