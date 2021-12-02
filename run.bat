f6355a2dfc18e514955195619c8cd0043d875997
Catelyn <github@catelyn.tech>
2021-12-02 11:26:54 +0100

    Fixed run script

--- run.bat
+++ run.bat
@@ -8,9 +8,13 @@ if "%1"=="" (
 cls
 
 if %1==all (
-    for %%s in (day??.c) do (
-        gcc %%~ns.c -o %%~ns.exe -Wpedantic -Wall -Werror && %%~ns
+    for /d %%s in (day??) do (
+        cd %%~ns
+        gcc %%~ns.c -o %%~ns.exe -Wpedantic -Wall -Werror -I../ && %%~ns
+        cd ../
     )
 ) else (
-    gcc %1.c -o %1.exe -Wpedantic -Wall -Werror && %1
+    cd %1
+    gcc %1.c -o %1.exe -Wpedantic -Wall -Werror -I../ && %1
+    cd ../
 )
