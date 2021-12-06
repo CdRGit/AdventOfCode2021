54ff66329af4e89ec75400873138078b9e400eec
Catelyn <github@catelyn.tech>
2021-12-06 10:56:38 +0100

    removed pedantic shit cuz it wouldn't let me print 64 bit integers otherwise

--- run.bat
+++ run.bat
@@ -8,12 +8,12 @@ if "%1"=="" (
 if %1==all (
     for /d %%s in (day??) do (
         cd %%~ns
-        gcc %%~ns.c -o %%~ns.exe -Wpedantic -Wall -Werror -I../ && %%~ns
+        gcc %%~ns.c -o %%~ns.exe -I../ && %%~ns
         echo[
         cd ../
     )
 ) else (
     cd %1
-    gcc %1.c -o %1.exe -Wpedantic -Wall -Werror -I../ && %1
+    gcc %1.c -o %1.exe -I../ && %1
     cd ../
 )
